a85accdd106e0ffae823d1c5997128de4545c7fc
Prof-KingKong <korbi.groeger@web.de>
2026-02-19 14:23:47 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAKJYtt8rToORI6UldcMD8dPRramGRg2ac",
+  authDomain: "quizbuzzer-5e2fc.firebaseapp.com",
+  databaseURL: "https://quizbuzzer-5e2fc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "quizbuzzer-5e2fc",
+  storageBucket: "quizbuzzer-5e2fc.firebasestorage.app",
+  messagingSenderId: "803724601105",
+  appId: "1:803724601105:web:48e42ee3543d5528278eb0"
+};
+
+window.QUIZ_ROOM = "default";
+window.BUZZER_COUNTDOWN_SECONDS = 3;
